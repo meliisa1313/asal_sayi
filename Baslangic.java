919c9b58e3e344707a5303faddb088701b19cb2b
meliisa1313 <65404152+meliisa1313@users.noreply.github.com>
2022-03-16 21:35:23 +0300

    Add files via upload

---
+++ Baslangic.java
@@ -0,0 +1,82 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        /* *******************
+         *****************
+         ***************
+         *************
+         ***********
+         *********
+         *******
+         *****
+         ***
+         *
+         * */
+
+       //int count = 0;
+        for (int s1 = 2; s1 <= 100; s1++) {
+            int x = 0;
+            for (int i = 2; i < s1; i++) {
+                if (s1 % i == 0) {
+                    x = 1;
+                    break;
+                }
+            }
+            if (x == 0) {
+                System.out.print(s1 + ",");
+                //xcount++;
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
